5c31d45ef2dff2f3ecec0395991add2ed1e6f8ac
Ethan J. Hadley <ethanscharlie@protonmail.com>
2026-04-04 12:01:49 -0600

    Created test stubs

---
+++ programming-project-3-optimal-selection/src/test/java/io/github/ethanscharlie/DynamicPayloadBuilderTest.java
@@ -0,0 +1,22 @@
+package io.github.ethanscharlie;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class DynamicPayloadBuilderTest {
+    @Test
+    public void testMachingList() {
+        assertTrue(false);
+    }
+
+    @Test
+    public void testEmptyList() {
+        assertTrue(false);
+    }
+
+    @Test
+    public void testListWithOneElement() {
+        assertTrue(false);
+    }
+}
